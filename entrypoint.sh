da316d45aed15e41699c368b048ea19f01a49f0b
4nx <simon.krenz@mailbox.org>
2018-05-05 01:56:52 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,11 @@ fi
 
 MOSQUITTO_UID=$(id -u mosquitto)
 MOSQUITTO_GID=$(id -g mosquitto)
-
 NEW_USER_ID=${USER_ID:-100}
 NEW_GROUP_ID=${GROUP_ID:-101}
+
 echo "Starting with mosquitto user id: ${NEW_USER_ID} and group id: ${NEW_GROUP_ID}"
+
 if [[ ${NEW_USER_ID} -ne ${MOSQUITTO_UID} || ${NEW_GROUP_ID} -ne ${MOSQUITTO_GID} ]]; then
     echo "Change group id of mosquitto to id ${NEW_GROUP_ID}"
     groupmod -g ${NEW_GROUP_ID} mosquitto
